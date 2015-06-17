56a4a7e736186c6733fa92cab83a77839f0d8405
Victor <bocharsky.bw@gmail.com>
2015-06-17 16:46:20 +0300

    Add dependencies

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
 
-echo 'Hello World!';
+require __DIR__ . '/vendor/autoload.php';
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// create a log channel
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('logs/log.log', Logger::WARNING));
+
+// add records to the log
+$log->addWarning('Foo');
+$log->addError('Bar');
